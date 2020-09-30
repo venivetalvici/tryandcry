682661018116e03cea8e1adeabeafaed1a6a5ce7
Vitalii <vvagusenko@gmail.com>
2020-09-30 18:02:21 +0200

    Day17 Homework

---
+++ src/HomeWork/ternar1.java
@@ -0,0 +1,21 @@
+package HomeWork;
+
+public class ternar {
+    public static void main(String[] args) {
+        System.out.println(getMeMax(15, 5)); //→ 15
+        System.out.println(getMeMax(4, 16)); //→ 16
+        System.out.println(getMeMax(20, 100)); //→ 100
+
+    }
+
+    private static String getMeMax(int a, int b) {
+        String result = "";
+        result = String.valueOf((a > b) ? +a : +b);
+        return "Большее число: " +  result;
+
+    }
+
+
+}
+    
+
