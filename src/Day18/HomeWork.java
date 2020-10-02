120dffdb3da7527096b0483b0d3523b4737f8361
Vitalii <vvagusenko@gmail.com>
2020-10-02 11:03:48 +0200

    Day18

--- src/Day18/HomeWork.java
+++ src/Day18/HomeWork.java
@@ -6,8 +6,8 @@ public class HomeWork {
 
     public static void main(String[] args) {
         //1 час = 1 кв метру done
-        //час стоит 27 евро без ндс
-        //1 плитка 30 на 20 см
+        //час стоит 27 евро без ндс done
+        //1 плитка 30 на 20 см (по условию) заменили на сканер
         //ввести стоимость плитки за кв метр done
         //вывести количество целых и не целых плиток
         //стоимость материала done
@@ -15,7 +15,7 @@ public class HomeWork {
         Scanner input = new Scanner(System.in);//вызываем Сканер
         System.out.println("Введите ширину рабочего участка ? ");
         double widtharea = input.nextDouble(); // получаем ширину в метрах не квадратных
-        System.out.println("Ширина рабочего участка:" + widtharea + " метра");
+        System.out.println("Ширина рабочего участка: " + widtharea + " метра");
         Scanner input2 = new Scanner(System.in);
         System.out.println("Введите длинну рабочего участка? ");
         double longarea = input2.nextDouble();// получаем длинну в метрах не квадратных
@@ -30,16 +30,31 @@ public class HomeWork {
         double squaretile = widthtile * longtile;//считаем площадь плитки
         Scanner input5 = new Scanner(System.in);
 
-        System.out.println("Как будем класть плитку? ");
+        System.out.println("Как будем класть плитку? Вдоль или поперек? ");
         String metod = input5.nextLine();
         if (metod.equals("вдоль")) {
+            double lengthResult =(long) longarea / longtile;
+            double widthResult = (long) widtharea/ widthtile;
+            double fullTiles = (long)lengthResult * widthResult;
+            double lengthResultCut =(long) longarea % longtile;
+            double widthResultCut = (long)widtharea % widthtile;
+            double cutTiles = (long)lengthResultCut + widthResultCut;
             System.out.println("Плитку удобнее класть вдоль (по длинне)");
-        } else if (metod.equals("поперек")) {
-            System.out.println("Плитку удобнее класть поперек (по ширине)");
-            //System.out.println(metod);// проверяю на всякий случай что в медоде
+            System.out.println("Целых плиток: " + fullTiles);
+            System.out.println("Плиток придется резать: " + cutTiles);
 
-            //if (metod.equals("вдоль")) {}
-             //tilearrangement  //выбираем как будем класть плитку
+        } else if (metod.equals("поперек")) {
+            double lengthResult = longarea / longtile;
+            double widthResult = widtharea/ widthtile;
+            double fullTiles = lengthResult * widthResult;
+            System.out.println("Целых плиток: " + fullTiles);
+            double lengthResultCut = longarea % longtile;
+            double widthResultCut = widtharea % widthtile;
+            double cutTiles = lengthResultCut + widthResultCut;
+            System.out.println("Плитку удобнее класть поперек (по ширине)");
+            System.out.println("Целых плиток: " + fullTiles);
+            System.out.println("Плиток придется резать: " + cutTiles);
+        }
             Scanner input6 = new Scanner(System.in);
             System.out.println("Стоимость квадратного метра плитки? ");
             double pricemetrtile = input6.nextDouble(); //получаем цену за квадратный метр плитки
@@ -54,17 +69,16 @@ public class HomeWork {
             System.out.println("Площадь участка : " + squarearea + " метров");
             System.out.println("Примерное время укладки плитки :" + (int) squarearea + " часов");
             System.out.println("Площадь плитки:" + squaretile);
-            //System.out.println("Количество целых плиток:" +);
             System.out.println("Цена материала : " + pricetile + " Euro");
             System.out.println("Стоимость работы за квадратный метр без НДС: " + pricehour + " Euro");
             System.out.println("Стоимость работ без НДС: " + pricejob + " Euro");
-            System.out.println("Итоговая стоимость" + price + " Euro");
+            System.out.println("Итоговая стоимость :" + price + " Euro");
 
         }
     }
 
 
-}
+
 
 
 
